6544aa3bc0a049c4608ec92726771cf4079a4318
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-11-26 17:32:20 +0100

    Delete the comments in migration files

--- migrations/Version20211126155904.php
+++ migrations/Version20211126155904.php
@@ -19,13 +19,11 @@ final class Version20211126155904 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD token VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP token');
     }
 }
