e1f334182deebf33743b3f1ed736cfc27308aea9
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-11-29 16:05:08 +0100

    Create actions to have the list of clients and one client by his id

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,20 +2,49 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * @Route("/clients")
+ */
 class ClientController extends AbstractController
 {
-    /**
-     * @Route("/client", name="client")
-     */
-    public function index(): JsonResponse
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ClientController.php',
-        ]);
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @Route(name="api_client_list", methods={"GET"})
+     */
+    public function collection(ClientRepository $clientRepository): JsonResponse
+    {
+        return new JsonResponse(
+            $this->serializer->serialize($clientRepository->findAll(), "json"),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="api_client_item", methods={"GET"})
+     */
+    public function item(int $id, ClientRepository $clientRepository): JsonResponse
+    {
+        $client = $clientRepository->findOneBy(['id' => $id]);
+
+        return new JsonResponse(
+            $this->serializer->serialize($client, "json"),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
     }
 }
