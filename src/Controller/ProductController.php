87d4780d0eaef432006e2bfb9e7d4e72a5f6364e
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-11-29 15:19:20 +0100

    Create action to get one product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * @Route("/products")
+ */
 class ProductController extends AbstractController
 {
     private $serializer;
@@ -18,12 +22,27 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/products", name="api_product_list", methods={"GET"})
+     * @Route(name="api_product_list", methods={"GET"})
      */
     public function collection(ProductRepository $productRepository): JsonResponse
     {
         return new JsonResponse(
             $this->serializer->serialize($productRepository->findAll(), "json"),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="api_product_item", methods={"GET"})
+     */
+    public function item(int $id, ProductRepository $productRepository): JsonResponse
+    {
+        $product = $productRepository->findOneBy(['id' => $id]);
+
+        return new JsonResponse(
+            $this->serializer->serialize($product, "json"),
             JsonResponse::HTTP_OK,
             [],
             true
