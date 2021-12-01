88c7f3065475be08dfc9ffd1754ac6a897c1e45e
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-12-01 16:46:58 +0100

    Re-add the password attribute to User class : we need it to create a token !

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,15 +6,25 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class UserFixtures extends Fixture
 {
+    private $hasher;
+
+    public function __construct(PasswordHasherFactoryInterface $factory)
+    {
+        $user = new User();
+        $this->hasher = $factory->getPasswordHasher($user);
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user
             ->setUsername('User1')
             ->setEmail('floryss.devweb+1@gmail.com')
+            ->setPassword($this->hasher->hash('secret'))
             ->setCreatedAt(new DateTimeImmutable())
             ->setToken('token123')
         ;
@@ -24,6 +34,7 @@ class UserFixtures extends Fixture
         $user2
             ->setUsername('User2')
             ->setEmail('floryss.devweb+2@gmail.com')
+            ->setPassword($this->hasher->hash('secret'))
             ->setCreatedAt(new DateTimeImmutable())
             ->setToken('token123')
         ;
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -28,6 +29,11 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="datetime_immutable")
@@ -75,6 +81,18 @@ class User
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
